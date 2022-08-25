16169b8ecf32c87b73a97b74d0ffa76f1ce7b924
karyna-khorshunova <karyna.khorshunova@gmail.com>
2022-08-25 13:40:47 +0300

    Add v1 for website

---
+++ src/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    function toggleNavigationMenu() {
+        $("#js-top-line").toggleClass("active");
+        $("#js-center-line").toggleClass("active");
+        $("#js-bottom-line").toggleClass("active");
+        $("#js-nav").toggleClass("show");
+    }
+
+    function onScroll() {
+        var scrollPos = $(document).scrollTop();
+        $('nav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('nav ul li a').removeClass("nav__item_active");
+                currLink.addClass("nav__item_active");
+            }
+        });
+    }
+
+    $("#js-hamburger").click(toggleNavigationMenu);
+
+    $('#show-more').click(function () {
+        var text = $('.about-me__additional-text');
+        if (text.hasClass('about-me__additional-text_hidden')) {
+
+            $('#show-more').text('Показати менше')
+            text.slideDown('slow');
+            text.removeClass('about-me__additional-text_hidden');
+        } else {
+            $('#show-more').text('Показати більше')
+            text.slideUp('slow');
+            text.addClass('about-me__additional-text_hidden');
+        }
+    });
+
+    $(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('nav__item_active');
+        })
+
+        if ($(this).text() !== 'Почати') {
+            $(this).addClass('nav__item_active');
+        }
+
+        if ($("#js-nav").hasClass('show')) {
+            toggleNavigationMenu();
+        }
+
+        var target = this.hash;
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 70
+        }, 500, 'swing', function () {
+            $(document).on("scroll", onScroll);
+        });
+    });
+})
+;
---
+++ src/swiper.js
@@ -0,0 +1,64 @@
+(function () {
+    const swiper = new Swiper('.swiper', {
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 60,
+            },
+            769: {
+                slidesPerView: 2,
+                spaceBetween: 60
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 60
+            },
+            1440: {
+                slidesPerView: 4,
+                spaceBetween: 60
+            }
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        zoom: true,
+        allowTouchMove: false,
+    });
+
+    function openFullscreenSwiper(imgSrc, imgAlt) {
+        var fullScreenSwiper = $('.fullscreen-swiper');
+        var mainSwiperMarkup = fullScreenSwiper.html();
+
+        fullScreenSwiper
+            .addClass('fullscreen-swiper_open')
+            .append(mainSwiperMarkup + `<div class='img-holder_fullscreen'><img src="${imgSrc}" alt="${imgAlt}"/> </div>`)
+            .fadeIn();
+
+        $('.fullscreen-swiper-backdrop').fadeIn();
+
+        $('.fullscreen-swiper').click(function (e) {
+            if(!e.target.classList.contains('img-holder_fullscreen')) {
+                $(this).hide().empty().removeClass('fullscreen-swiper_open');
+                $('.fullscreen-swiper-backdrop').fadeOut();
+            }
+        })
+
+        $('.fullscreen-swiper-close').on('click', function () {
+            $('.fullscreen-swiper').hide().empty().removeClass('fullscreen-swiper_open');
+            $('.fullscreen-swiper-backdrop').fadeOut();
+        });
+    }
+
+    $('.swiper .swiper-slide').on('click', function () {
+        var imgSrc = $(this).find('img').attr('src')
+        var imgAlt = $(this).find('img').attr('alt')
+        openFullscreenSwiper(imgSrc, imgAlt);
+    });
+
+})();
